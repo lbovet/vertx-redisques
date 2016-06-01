60c17dd533cf204a7f9efd3223b4e4161ec7cfed
floriankammermann <florian@kammermann.me>
2016-06-01 08:27:05 +0200

    moved junit timeout to child classes to allow different timeouts

--- src/test/java/org/swisspush/redisques/AbstractTestCase.java
+++ src/test/java/org/swisspush/redisques/AbstractTestCase.java
@@ -1,46 +1,28 @@
 package org.swisspush.redisques;
 
 import io.vertx.core.*;
-import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.Timeout;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.swisspush.redisques.util.RedisquesConfiguration;
 import redis.clients.jedis.Jedis;
 
-import javax.xml.bind.DatatypeConverter;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static org.swisspush.redisques.util.RedisquesAPI.*;
-
 @RunWith(VertxUnitRunner.class)
 public abstract class AbstractTestCase {
-
-
-
-
-    @Rule
-    public Timeout rule = Timeout.seconds(5);
 
     static Logger log = LoggerFactory.getLogger(AbstractTestCase.class);
 
     protected static Vertx vertx;
     protected static Jedis jedis;
+
+    protected static String deploymentId = "";
 
     protected void flushAll(){
         if(jedis != null){
@@ -61,7 +43,7 @@ public abstract class AbstractTestCase {
         if(!RedisEmbeddedConfiguration.useExternalRedis()) {
             RedisEmbeddedConfiguration.redisServer.start();
         }
-        setUp(context);
+        deployRedisques(context, 2);
     }
 
     @AfterClass
@@ -72,27 +54,22 @@ public abstract class AbstractTestCase {
         jedis.close();
     }
 
-    private static void setUp(TestContext context) {
+    protected static void deployRedisques(TestContext context, int refreshPeriod) {
         vertx = Vertx.vertx();
-        initProcessor(vertx.eventBus());
 
         JsonObject config = RedisquesConfiguration.with()
                 .processorAddress("processor-address")
                 .redisEncoding("ISO-8859-1")
-                .refreshPeriod(2)
+                .refreshPeriod(refreshPeriod)
                 .build()
                 .asJsonObject();
 
         RedisQues redisQues = new RedisQues();
         vertx.deployVerticle(redisQues, new DeploymentOptions().setConfig(config), context.asyncAssertSuccess(event -> {
+            deploymentId = event;
             log.info("vert.x Deploy - " + redisQues.getClass().getSimpleName() + " was successful.");
             jedis = new Jedis("localhost", 6379, 5000);
         }));
-    }
-
-    private static void initProcessor(EventBus eventBus){
-
-
     }
 
     @Before
